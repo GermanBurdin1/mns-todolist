190ebe2a673199d5f9405c6fd6cf68e5878488d9
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-02-27 15:28:36 +0100

    ajoutera la suppression des taches

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 import { addTask, renderTasks } from './modules/tasks.js';
 import { setupDragAndDrop } from './modules/dragAndDrop.js';
+import { saveTasks } from './modules/storage.js'; // добавлен импорт loadTasks и saveTasks
 
 document.addEventListener('DOMContentLoaded', () => {
     renderTasks(); 
     
     const inputField = document.querySelector('.input-section .task-input');
     const addTaskButton = document.getElementById('addTaskButton');
+    const deleteTaskButton = document.getElementById('deleteTaskButton'); // добавлена переменная для кнопки удаления задач
 
     inputField.addEventListener('keypress', (e) => {
         if (e.key === 'Enter') {
@@ -17,6 +19,15 @@ document.addEventListener('DOMContentLoaded', () => {
     addTaskButton.addEventListener('click', () => {
         addTask(inputField.value, '.todo3 .droppable', 'tache-a-faire'); 
         inputField.value = '';
+    });
+
+    deleteTaskButton.addEventListener('click', () => { // добавлен обработчик события для кнопки удаления задач
+        const categories = ['tache-a-faire', 'tache-en-cours', 'tache-accomplie'];
+        categories.forEach(category => {
+            saveTasks([], category); // очищаем хранилище для каждой категории задач
+            const container = document.querySelector(`.droppable[data-category="${category}"]`);
+            container.innerHTML = ''; // очищаем контейнер в интерфейсе
+        });
     });
 
     setupDragAndDrop();
